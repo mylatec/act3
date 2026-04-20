67c12cddb5b8404470bc26cd3fd8c81cc9808226
mylatec <ignacio.gonzalez@tecmilenio.mx>
2026-04-20 08:25:23 -0600

    Version 4

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 
-   return jsonify({"message":"¡Hello from Flask in Docker. Version 3!"})
+   return jsonify({"message":"¡Hello from Flask in Docker. Version 4. This change will be displayed in 5 minutes more or less!"})
 
 if __name__=="__main__":
    app.run(host="0.0.0.0", port=5000)
